cf957c70390740c1f8d7e3f79d70ad4fa67b54e0
Pavel Kroh <pavelkroh@smartlook.com>
2022-10-09 11:19:19 +0200

    Release 2.1.0 build 155

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SmartlookAnalytics",
-            url: "https://sdk.smartlook.com/ios/smartlook-ios-sdk-2.0.3.147.zip",
-            checksum: "3213e28ab73b36f0b9c05b7cdc0ede959ec760f02a36ff2730cc8b7ef9ff8ea3"
+            url: "https://sdk.smartlook.com/ios/smartlook-ios-sdk-2.1.0.155.zip",
+            checksum: "62d3fa7d70fb8dd64ed318b044d3f3ae8aa096072c3460322a98e5d660c891c2"
         ),
     ]
 )
