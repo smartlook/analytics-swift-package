b20215e2cddda2402d769d7d2bda49394364bbd5
Pavel Kroh <pavelkroh@smartlook.com>
2022-11-08 11:27:32 +0100

    Another beta

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SmartlookAnalytics",
-            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-2.1.1.163.zip",
-            checksum: "8a604844b1c3aea83e44d41294390ba2b20b215789455c477ae1c5701d46b0ea"
+            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-2.1.1.164.zip",
+            checksum: "4f557104b54bec6063f0821869ccbdd943c0a6c652a6d8e8452fe7a18056b2f7"
         ),
     ]
 )
