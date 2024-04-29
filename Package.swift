b86b3cd392cb9c404c3602e9b6d4242f5ee084ac
Martin Budínský <budim@cisco.com>
2024-04-29 15:23:05 +0200

    chore: release 2.2.11

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SmartlookAnalytics",
-            url: "https://sdk.smartlook.com/ios/smartlook-ios-sdk-2.2.10.247.zip",
-            checksum: "ca5a8feeb988170d82eef2b3041b9b8a7fd7fdce7fec87d7968451576baef0d9"
+            url: "https://sdk.smartlook.com/ios/smartlook-ios-sdk-2.2.11.248.zip",
+            checksum: "a42c71fec1783088788dc1a3953a082dbb74a76d78aa7aef8022ea11e54ebb16"
         ),
     ]
 )
